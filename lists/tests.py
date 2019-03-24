62e7bbc8f5c85428b124cf5c203b2cf565c53925
Akagilnc <akagilnc@gmail.com>
2019-03-24 19:12:43 +0900

    Update tests.py

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.http import HttpRequest
-from django.urls import reverse
+from django.urls import resolve
 from django.template.loader import render_to_string
 from lists.views import home_page
 
@@ -10,7 +10,7 @@ from lists.views import home_page
 class HomePageTest(TestCase):
 
     def test_root_url_resolves_to_home_page_view(self):
-        found = reverse('/')
+        found = resolve('/')
         self.assertEqual(found.func, home_page)
 
     def test_home_page_returns_correct_html(self):
